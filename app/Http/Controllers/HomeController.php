e6e990757454834ffc24e4e47184830e3f57e0f8
painteyes <andreantoniocchipinti@gmail.com>
2022-03-22 11:22:14 +0100

    aggiunta la rotta per l'homepage pubblica

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,21 +8,18 @@ class HomeController extends Controller
 {
     /**
      * Create a new controller instance.
-     *
      * @return void
-     */
-    public function __construct()
+    */
+
+    public function home()
     {
-        $this->middleware('auth');
+        return view('pages.guests.welcome');
     }
 
     /**
      * Show the application dashboard.
-     *
      * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
+    */
+
+    
 }
