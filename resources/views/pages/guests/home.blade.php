b06747d25db961478a1a12a069f7603f619ab2da
salvomel <salv.mel@gmail.com>
2022-03-26 10:41:15 +0100

    homepage-style

--- resources/views/pages/guests/home.blade.php
+++ resources/views/pages/guests/home.blade.php
@@ -4,22 +4,23 @@
 @section('content')
 
         {{-- Jumbotron --}}
-        
-        <div class="hero text-center">
-                <div class="container-hp">
-
-                   <div class="display-1 sfondo">
-                     <div class="main-logo">
-                      
+        <div class="jumbotron-del">
+                <div class="container">
+                    <div class="d-flex row-cols-2">
+                        <div class="col">
+                            <div class="d-flex flex-column">
+                                <span class="title-jumbo">
+                                    I piatti che ami, a domicilio.
+                                    Cerchi qualcosa?
+                                </span>
+                            </div>
+                        </div>
+                        <div class="col text-center pt-3">
+                            <img src="/images/jumbotron.svg" alt="jumbo-img">
+                        </div>
                     </div>
-                    <div class="main-headline">
-                      <h1>I piatti che ami, a domicilio.</h1>
-                      <h3>Cerchi qualcosa?</h3>
-                    </div>
-                  </div>
-                  
                 </div>
-        </div>
+            </div>
 
         {{-- Componente Vue per ricercare i ristoranti --}}
         <search-restaurants 
