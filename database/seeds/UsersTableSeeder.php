d0bd33dc447da42837194babf8c4ddaa3b323479
painteyes <andreantoniocchipinti@gmail.com>
2022-04-04 15:59:25 +0200

    commit changes

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -35,25 +35,25 @@ class UsersTableSeeder extends Seeder
         ]);
 
 
-        $user = new User();
+        // $user = new User();
 
-        $user->name = 'Kaori';
-        $user->email = 'kaori@gmail.com';
-        $user->password = $faker->password();
-        $user->phone_number = '3909847811';
-        $user->restaurant_name = 'Grazia e Graziella';
-        $user->address = 'Viale Battista Bardanzellu, 27';
-        $user->vat_number = $faker->randomNumber(9, true);
-        $user->img_path = 'restaurant-1.jpg';
-        $user->slug = User::getUniqueSlugFromTitle($user->restaurant_name);
-        $user->description = 'Il ristorante Grazia e Graziella in pochi anni è riuscito ad affermarsi come attività di riferimento per la zona e non solo.';
-        $user->save();
+        // $user->name = 'Kaori';
+        // $user->email = 'kaori@gmail.com';
+        // $user->password = $faker->password();
+        // $user->phone_number = '3909847811';
+        // $user->restaurant_name = 'Grazia e Graziella';
+        // $user->address = 'Viale Battista Bardanzellu, 27';
+        // $user->vat_number = $faker->randomNumber(9, true);
+        // $user->img_path = 'restaurant-1.jpg';
+        // $user->slug = User::getUniqueSlugFromTitle($user->restaurant_name);
+        // $user->description = 'Il ristorante Grazia e Graziella in pochi anni è riuscito ad affermarsi come attività di riferimento per la zona e non solo.';
+        // $user->save();
 
-        $user->categories()->sync([ 
-            rand(1, 10),
-            rand(1, 10),
-            rand(1, 10),
-        ]);
+        // $user->categories()->sync([ 
+        //     rand(1, 10),
+        //     rand(1, 10),
+        //     rand(1, 10),
+        // ]);
 
 
 
